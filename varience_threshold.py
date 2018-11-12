df79f11df4ed5e024bd644201cbac82bf5f9b415
ryanarj <karshan813@gmail.com>
2018-11-12 16:17:38 -0500

    Create the varience threshold algo

--- varience_threshold.py
+++ varience_threshold.py
@@ -1,7 +1,13 @@
 # File for the varience threshold feature selection algorithm
+import numpy as np
+from sklearn.feature_selection import VarianceThreshold
 
 
-def get_features():
-    # TODO
-    print("NOT IMPLEMENTED")
+def varience_threshold(data):
+    varience = np.var(data)
+    m = data - np.mean(varience)
+    vt = VarianceThreshold(threshold=m)
+    fit_data = vt.fit_transform(data)
+    return fit_data
+
 
